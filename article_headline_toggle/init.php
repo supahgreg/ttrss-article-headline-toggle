ff364bae9839a162f2b8ccdb6279f7f7f7c425de
Greg Parris <greg.parris@gmail.com>
2018-12-02 14:23:05 -0600

    v1.4 for compatibility with the latest tt-rss

--- article_headline_toggle/init.php
+++ article_headline_toggle/init.php
@@ -6,7 +6,7 @@ class Article_Headline_Toggle extends Plugin {
 
   function about() {
     return [
-      1.3, // version
+      1.4, // version
       'Toggle article visibility by clicking on the headline', // description
       'wn', // author
       false, // is system
@@ -22,55 +22,45 @@ class Article_Headline_Toggle extends Plugin {
 
   function init($host) {
     $this->host = $host;
-
-    //$host->add_hook($host::HOOK_PREFS_TAB, $this);
-    //$host->add_hook($host::HOOK_PREFS_TAB_SECTION, $this);
   }
 
 
-  /**
-   * Give a hint when hovering over an article's headline.
-   */
   function get_css() {
     return '#headlines-frame > div span.titleWrap { cursor: pointer; }';
   }
 
 
-  /**
-   * Wrapping the cdmClicked function to toggle an article
-   * when its headline is clicked.
-   *
-   * TODO: Add a pref to control whether pre-expanded articles
-   * (from "Automatically expand articles in combined mode")
-   * are eligible to be toggled.
-   */
   function get_js() {
     return <<<'JS'
-;(() => {
-  // Do nothing if the user is forcing the expanded view
-  if (getInitParam('cdm_expanded')) return;
+require(['dojo/ready'], (ready) => {
+  ready(() => {
+    PluginHost.register(PluginHost.HOOK_RUNTIME_INFO_LOADED, () => {
+      // Do nothing if the user is forcing the expanded view
+      if (App.getInitParam('cdm_expanded')) return;
 
-  window.cdmClicked = (aEvent, aId, aInBody) => {
-    const id = aEvent.target.dataset.articleId || aEvent.target.parentNode.dataset.articleId;
+      Headlines.click = (aEvent /*, aId, aInBody*/) => {
+        const id = aEvent.target.dataset.articleId || aEvent.target.parentNode.dataset.articleId;
 
-    if (!id || aEvent.ctrlKey) {
-      return true;
-    }
+        if (!id || aEvent.ctrlKey) {
+          return true;
+        }
 
-    if (document.getElementById(`RROW-${id}`).classList.contains('active')) {
-      if (aEvent.target.tagName === 'A') {
-        return true;
-      }
-      cdmCollapseActive(aEvent);
-    }
-    else {
-      setActiveArticleId(id);
-      cdmScrollToArticleId(id);
-    }
+        if (document.getElementById(`RROW-${id}`).classList.contains('active')) {
+          if (aEvent.target.tagName === 'A') {
+            return true;
+          }
+          Article.cdmUnsetActive(aEvent);
+        }
+        else {
+          Article.setActive(id);
+          Article.cdmScrollToId(id);
+        }
 
-    return false;
-  };
-})();
+        return false;
+      };
+    });
+  });
+});
 JS;
   }
 }
